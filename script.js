29e74e6bb9a11406b67834dc29fde5f0f97aa060
endriu17 <aolaszewski@gmail.com>
2018-09-05 10:03:50 +0200

    add files to repo

---
+++ script.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('index.html', function(err, content){
+            response.writeHead(200, {'Content-type':'text/html'});
+            response.write(content);
+            response.end();
+        });
+    } else {
+        fs.readFile('404.html', function(error, content) {
+            response.writeHead(200, {'Content-type':'text/html'});
+            response.write(content);
+            response.end();
+        });
+    }
+});
+
+server.listen(9000);
+
+console.log("Listening on port 9000");
